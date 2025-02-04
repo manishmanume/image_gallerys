db335ac799c5010fc7cc11409983350ef5e37395
manishmanume <149936975+manishmanume@users.noreply.github.com>
2025-02-04 23:36:31 +0530

    change css and gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import ArtCard from './ArtCard';
 import '../assets/CSS/Gallery.css';
+import art1 from '../assets/Images/1.jpg';
+import art2 from '../assets/Images/2.jpg';
+import art3 from '../assets/Images/3.jpg';
+import art4 from '../assets/Images/4.jpg';
+import art5 from '../assets/Images/5.jpg';
+import art6 from '../assets/Images/6.jpg';
+import art7 from '../assets/Images/7.jpg';
+import art8 from '../assets/Images/8.jpg';
 
 const Gallery = () => {
   const artworks = [
-    { src: '../../public/Images/1.jpg', alt: 'Artwork 1' },
-    { src: '../../public/Images/2.jpg', alt: 'Artwork 2' },
-    { src: '../../public/Images/3.jpg', alt: 'Artwork 3' },
-    { src: '../../public/Images/4.jpg', alt: 'Artwork 4' },
-    { src: '../../public/Images/5.jpg', alt: 'Artwork 5' },
-    { src: '../../public/Images/6.jpg', alt: 'Artwork 6' },
-    { src: '../../public/Images/7.jpg', alt: 'Artwork 7' },
-    { src: '../../public/Images/8.jpg', alt: 'Artwork 8' },
+    { src: art1, alt: 'Artwork 1' },
+    { src: art2, alt: 'Artwork 2' },
+    { src: art3, alt: 'Artwork 3' },
+    { src: art4, alt: 'Artwork 4' },
+    { src: art5, alt: 'Artwork 5' },
+    { src: art6, alt: 'Artwork 6' },
+    { src: art7, alt: 'Artwork 7' },
+    { src: art8, alt: 'Artwork 8' },
   ];
-  
+
   return (
     <div className="gallery-container">
       <div className="gallery-header">
